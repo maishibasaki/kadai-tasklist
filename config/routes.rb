64022d1bc4beefba64670d64f536b93a7b78c397
Mai Shibasaki <mai.shibasaki@outlook.jp>
2017-04-26 04:05:56 +0000

    twitter tasklist

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
 
-  get 'users', to: 'users#index'
-  post 'users', to: 'users#create'
-  get 'users/new', to: 'users#new'
-  get 'users/:id', to: 'users#show'
-  post 'tasks', to: 'tasks#create'
-  patch 'tasks/:id', to: 'tasks#update'
-  put 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#destroy'
-
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
